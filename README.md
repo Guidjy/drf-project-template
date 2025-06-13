456b83c23703d07d04d42e7ca65bda53f1b1e91c
Guidjy <guippimentel@gmail.com>
2025-06-13 12:35:05 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # drf-project-template
 django rest framework project template with a registry app and generic model crud capabilities
+
+# packages:
+1. django
+2. djangorestframework
+3. django-cors-headers
+4. email-validator
+```pip install django djangorestframework django-cors-headers email-validator```
---
+++ backend/registry/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.get_all_users),
+    path('register/', views.register_user),
+    path('login/', views.login_user),
+    path('logout/', views.logout_user),
+    path('edit_user/', views.edit_user),
+    path('delete_user/<int:id>', views.delete_user),
+    path('profile/', views.user_profile),
+]
